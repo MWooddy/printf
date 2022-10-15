6123b828a45432d9c0f307c18d0d44dc2b4d2b6e
Mike W <mwwoody9@gmail.com>
2022-10-15 01:08:25 -0700

    Printf project layout

---
+++ main.h
@@ -0,0 +1,21 @@
+/* Defining header file , putting it in and ifdefine/endif
+#ifndef MAIN_H
+#define MAIN_H
+*/
+
+/* Libaries
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+*/
+
+/* prototypes for _printf functions */
+int _printf(const char *format, ...); /*Provided Proto Function*/
+int _printChar( /*need to think thr arguments to pass*/);
+int _printString( /*need to think thr arguments to pass*/);
+int _printNumber( /*need to think thr arguments to pass*/);
+int _printRot13( /*need to think thr arguments to pass*/);
+int _printRevS( /*need to think thr arguments to pass*/);
+
+
+/*#endif*/
